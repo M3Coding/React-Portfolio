37be4883ce3dad89216bedec61d708a21e0d534e
M3Coding <meady2009@gmail.com>
2023-10-03 18:16:30 -0400

    initial commit

---
+++ portfolio/src/components/Card.js
@@ -0,0 +1,13 @@
+import { Heading, HStack, Image, Text, VStack} from "@chakra-ui/react";
+// import {FontAwesomeIcon} from "@fortawesome/react-fortawesome";
+// import { faArrowRight } from "@fortawesome/free/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({title, description, imageSrc }) => {
+    // Implement the UI for the Card componet according to the instructions. 
+    // You should be able to implement the component with the elements imported above.
+    // Feel Free to import other UI components from Chakara UI if you wish to.
+    return null;
+}
+
+export default Card; 
